7f8873bd1e915296207a676a452be0dc77426047
JonnyAxehandle <channel14hq@gmail.com>
2015-08-30 17:06:07 -0400

    Cleaned up CraftListener

--- src/main/java/com/twidgysoft/rilncraft/crafting/CraftListener.java
+++ src/main/java/com/twidgysoft/rilncraft/crafting/CraftListener.java
@@ -1,12 +1,12 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * 
  */
 package com.twidgysoft.rilncraft.crafting;
 
 import com.twidgysoft.rilncraft.GainSkillEvent;
 import com.twidgysoft.rilncraft.SkillDefinition;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
@@ -17,7 +17,7 @@ import org.bukkit.event.inventory.InventoryAction;
 import org.bukkit.inventory.ItemStack;
 
 /**
- *
+ * Listens for items being crafted, awards players XP, and rolls for perks
  * @author Jonny
  */
 class CraftListener implements Listener {
@@ -27,30 +27,50 @@ class CraftListener implements Listener {
         this.module = module;
     }
     
+    /**
+     * Handles the event of an item being crafted on a grid
+     * @param e 
+     */
     @EventHandler
     public void onItemCrafted( CraftItemEvent e )
     {
+        ItemStack[] ingredientsUsed;
+        ItemStack itemCrafted;
+        int itemCraftedAmount;
+        Material itemCraftedType;
+        Player player;
+        
+        /* Make sure it's a player */
         if( !( e.getWhoClicked() instanceof Player ) )
         {
             return;
         }
         
-        ItemStack itemCrafted = e.getCurrentItem();
-        int itemCraftedAmount = itemCrafted.getAmount();
-        Material itemCraftedType = itemCrafted.getType();
-        Player player = (Player) e.getWhoClicked();
-        ItemStack[] matrix = e.getInventory().getMatrix();
+        itemCrafted = e.getCurrentItem();
+        itemCraftedAmount = itemCrafted.getAmount();
+        itemCraftedType = itemCrafted.getType();
         
-        if( module.isBlacklisted(itemCraftedType) || module.isBlacklisted(matrix) )
+        player = (Player) e.getWhoClicked();
+        
+        ingredientsUsed = e.getInventory().getMatrix();
+        
+        /* Make sure none of the items are on the blacklist */
+        if( module.isBlacklisted(itemCraftedType) || module.isBlacklisted(ingredientsUsed) )
         {
             return;
         }
         
+        /* Make sure something actually got made */
         InventoryAction action = e.getAction();
         switch( action )
         {
+            /* "NOTHING" refers to a craft action that failed, such as clicking
+            the item in the crafting result slot with a different item or non
+            stacking item */
             case NOTHING:
                 return;
+                    
+            /* Actions for a successful crafting */
             case PICKUP_ALL:
             case PICKUP_HALF:
             case PICKUP_ONE:
@@ -58,15 +78,30 @@ class CraftListener implements Listener {
             case DROP_ONE_SLOT:
             case DROP_ALL_SLOT:
                 awardCraftXP( player , itemCraftedType , itemCraftedAmount );
-                awardMaterialXP( player , matrix );
+                awardMaterialXP( player , ingredientsUsed );
                 return;
+            
+            /* Action for shift+clicking to craft */
             case MOVE_TO_OTHER_INVENTORY:
+                
+                // TODO: Figure out shift clicking
+                
                 return;
+                
+            /* In case we missed something, this will let us know */
             default:
-                return;
+                String msg = String.format("WARNING: InventoryAction '%s' unaccounted for",action.name());
+                player.sendMessage(msg);
+                Logger.getLogger(getClass().getName()).log(Level.WARNING,msg);
         }
     }
 
+    /**
+     * Awards the player XP for crafting an item
+     * @param player
+     * @param itemCraftedType
+     * @param itemCraftedAmount 
+     */
     private void awardCraftXP(Player player, Material itemCraftedType, int itemCraftedAmount) {
         ItemDefinition itemDefinition = module.getItemDefinition(itemCraftedType);
         if( itemDefinition == null )
@@ -88,6 +123,11 @@ class CraftListener implements Listener {
         Bukkit.getServer().getPluginManager().callEvent(e);
     }
 
+    /**
+     * Awards the player XP for using materials in a crafting matrix
+     * @param player
+     * @param matrix 
+     */
     private void awardMaterialXP(Player player, ItemStack[] matrix) {
         for( ItemStack item : matrix )
         {
@@ -95,6 +135,11 @@ class CraftListener implements Listener {
         }
     }
     
+    /**
+     * Awards the player XP for using a material
+     * @param player
+     * @param material 
+     */
     private void awardMaterialXP(Player player, Material material) {
         ItemDefinition itemDefinition = module.getItemDefinition(material);
         if( itemDefinition == null )
